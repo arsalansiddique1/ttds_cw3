a1511fdad18655ba1db5fcd2b6b13150c2d41e4d
filipgdorm <filip.dorm1@gmail.com>
2024-02-20 11:17:15 +0000

    made csv tab separated

--- caption_extraction/read_imags_v2.py
+++ caption_extraction/read_imags_v2.py
@@ -1,5 +1,6 @@
 from mwparserfromhtml import HTMLDump
 import csv
+import re
 
 html_file_path = "/Users/filipdorm/enwiki-NS0-20240201-ENTERPRISE-HTML.json.tar.gz"
 html_dump = HTMLDump(html_file_path)
@@ -8,7 +9,7 @@ csv_file_path = 'images_with_captions.csv'
 
 with open(csv_file_path, 'w', newline='', encoding='utf-8') as csv_file:
 
-    csv_writer = csv.writer(csv_file)
+    csv_writer = csv.writer(csv_file, delimiter='\t')
     csv_writer.writerow(['title', 'filenames', 'captions'])
 
     i = 0
@@ -17,15 +18,13 @@ with open(csv_file_path, 'w', newline='', encoding='utf-8') as csv_file:
         title = article.get_title()
         for image in article.html.wikistew.get_images():
             filename = image.title
-            cleaned_line = image.caption
+            
+            caption = image.caption
+            cleaned_line =  re.sub(r'\t', '', caption)
+
             if image.caption != "":     #only include images with a caption
                 i+=1
                 csv_writer.writerow([title, filename, cleaned_line])
                 print(f'Filename: {filename}, Caption: {image.caption}')
 
         if i == 10000:break
-
-   
-    
-    
-            
