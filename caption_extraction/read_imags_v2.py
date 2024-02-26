621b0cf83bd0bc037ca4329a7b986d8631fea9c0
Ivibae <ivibaeib@gmail.com>
2024-02-26 19:22:27 +0000

    Adding preprocessing, image ids and small dataset

--- caption_extraction/read_imags_v2.py
+++ caption_extraction/read_imags_v2.py
@@ -1,30 +1,57 @@
 from mwparserfromhtml import HTMLDump
 import csv
 import re
+from itertools import filterfalse
+from nltk.stem import PorterStemmer
 
-html_file_path = "/Users/filipdorm/enwiki-NS0-20240201-ENTERPRISE-HTML.json.tar.gz"
+html_file_path = "/Users/Ivibae/enwiki-NS0-20240201-ENTERPRISE-HTML.json.tar.gz"
 html_dump = HTMLDump(html_file_path)
 
-csv_file_path = 'images_with_captions.csv'
+csv_file_path = 'try.csv'
+
+
+def extract_stopwords(stopwords_file):
+    '''
+    Helper function to extract stopwords from a stopwords file
+    '''
+    if stopwords_file is None:
+        return []
+    with open(stopwords_file, 'r', encoding="utf-8") as s:
+        text = s.read().lower()
+        pattern = r'\b[\w\']+\b'
+        stopwords = re.findall(pattern, text)
+    return stopwords
+
+def preprocess_text(text, stopwords):
+    '''
+    Preprocess a single text: tokenization, stopping, case folding, stemming
+    '''
+    tokens = re.findall(r'\b[\w\']+\b', re.sub(r'_', ' ', text).lower())
+    tokens = list(filterfalse(stopwords.__contains__, tokens)) 
+    stemmer = PorterStemmer()
+    return [stemmer.stem(token) for token in tokens]
+
+image_id = 0
+
+
+stopwords = extract_stopwords("ttds_2023_english_stop_words.txt")
 
 with open(csv_file_path, 'w', newline='', encoding='utf-8') as csv_file:
 
     csv_writer = csv.writer(csv_file, delimiter='\t')
-    csv_writer.writerow(['title', 'filenames', 'captions'])
+    csv_writer.writerow(['title', 'filenames', 'image id', 'captions'])
 
-    i = 0
     for article in html_dump:
-        
         title = article.get_title()
         for image in article.html.wikistew.get_images():
-            filename = image.title
-            
-            caption = image.caption
-            cleaned_line =  re.sub(r'\t', '', caption)
 
             if image.caption != "":     #only include images with a caption
-                i+=1
-                csv_writer.writerow([title, filename, cleaned_line])
+                filename = image.title
+            
+                cleaned_caption = preprocess_text(image.caption, stopwords)
+                csv_writer.writerow([title, filename, image_id, ' '.join(cleaned_caption)])
                 print(f'Filename: {filename}, Caption: {image.caption}')
+                image_id += 1
 
-        if i == 10000:break
+        if image_id == 10000:
+            break
