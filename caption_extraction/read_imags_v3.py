f14654f4dd54f1f54568ccf0cdc7542bac860ad5
joe <63621767+joehutcheson@users.noreply.github.com>
2024-03-04 12:31:09 +0000

    Code to CONTINUE extracting captions after crash

---
+++ caption_extraction/read_imags_v3.py
@@ -0,0 +1,52 @@
+import sys
+
+from mwparserfromhtml import HTMLDump
+import re
+from itertools import filterfalse
+from nltk.stem import PorterStemmer
+from tqdm import tqdm
+
+html_file_path = sys.argv[1]
+html_dump = HTMLDump(html_file_path)
+
+import connect_connector
+import sqlalchemy
+
+db: sqlalchemy.engine.base.Engine = connect_connector.connect_with_connector()
+
+from hashlib import md5
+
+image_id = 0
+
+curr_max = 2606841  # == select max(id) from captions2;
+
+with db.connect() as conn:
+
+    stmt = sqlalchemy.text(
+        "INSERT INTO captions2 (id, filename, title, caption) VALUES (:id, :filename, :title, :caption)"
+    )
+
+    for article in tqdm(html_dump):
+        title = article.get_title()
+        for image in article.html.wikistew.get_images():
+
+            if image.caption != "":     #only include images with a caption
+                if image_id <= curr_max:
+                    image_id += 1
+                    continue
+
+                filename = image.title
+                f_hash = md5(filename.encode()).hexdigest()
+                file_loc = f_hash[0:1] + '/' + f_hash[0:2] + '/' + filename
+
+                caption = image.caption
+
+                conn.execute(stmt, parameters=
+                    {"id": image_id, "filename": file_loc, "title": title, "caption": caption}
+                )
+                conn.commit()
+
+                image_id += 1
+
+        # if image_id == 10000:
+        #     break
