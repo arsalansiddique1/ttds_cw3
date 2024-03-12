7cf91bb02f5ef06cb2a47138d008fd8c4b7e4aec
filipgdorm <filip.dorm1@gmail.com>
2024-03-12 16:32:05 +0000

    fix ui error no images

--- gui/src/API.js
+++ gui/src/API.js
@@ -1,5 +1,5 @@
 const API_URL = 'http://146.148.26.219:8000/';
-//const API_URL = 'http://127.0.0.1:8000/';
+// const API_URL = 'http://127.0.0.1:8000/';
 const BASE_URL = "https://upload.wikimedia.org/wikipedia/commons/";
 
 export default {
@@ -9,33 +9,49 @@ export default {
     return fetch(url)
       .then(response => response.json())
       .then(result => {
-        const data = result.results.map(item => ({
-          id: item.id,
-          url: BASE_URL + item.filename,
-          title: item.title,
-          caption: item.caption
-        }));
-        console.log(result)
-        console.log(data)
-        return data;
+        if (result && result.results) { // Check if result is not null and has results property
+          const data = result.results.map(item => ({
+            id: item.id,
+            url: BASE_URL + item.filename,
+            title: item.title,
+            caption: item.caption
+          }));
+          console.log(result);
+          console.log(data);
+          return data;
+        } else {
+          console.log("No data found"); // Handle case when no data is found
+          return [];
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error); // Log any errors that occur during the fetch
+        return []; // Return an empty array in case of error
       });
   },
   boolean_search(searchTerm) {
-    // Implement your boolean search logic here
-    // For example:
     const url = `${API_URL}boolean_search?query=${searchTerm}`;
     return fetch(url)
       .then(response => response.json())
       .then(result => {
-        const data = result.results.map(item => ({
-          id: item.id,
-          url: BASE_URL + item.filename,
-          title: item.title,
-          caption: item.caption
-        }));
-        console.log(result)
-        console.log(data)
-        return data;
+        if (result && result.results) { // Check if result is not null and has results property
+          const data = result.results.map(item => ({
+            id: item.id,
+            url: BASE_URL + item.filename,
+            title: item.title,
+            caption: item.caption
+          }));
+          console.log(result);
+          console.log(data);
+          return data;
+        } else {
+          console.log("No data found"); // Handle case when no data is found
+          return [];
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error); // Log any errors that occur during the fetch
+        return []; // Return an empty array in case of error
       });
   }
 };
