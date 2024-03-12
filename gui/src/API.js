1ac79221501e75da26d58906e58625f5fdcb5613
filipgdorm <filip.dorm1@gmail.com>
2024-03-12 14:59:23 +0000

    fixed api_url

--- gui/src/API.js
+++ gui/src/API.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://146.148.26.219:8000/search?query=';
+const API_URL = 'http://146.148.26.219:8000/';
 //const API_URL = 'http://127.0.0.1:8000/';
 const BASE_URL = "https://upload.wikimedia.org/wikipedia/commons/";
 
