a4f959dd3eee5d797e94349f43c222dc3e4a9d0e
arsalansiddique1 <arsalan1699@hotmail.com>
2024-03-14 14:48:27 +0000

    added LLM keyword query expanion code

--- LLM_query_expansion/zero_shot.py
+++ LLM_query_expansion/zero_shot.py
@@ -1,17 +1,38 @@
 from transformers import pipeline
 import pandas as pd
+import time
+from keywords_extraction import *
+
 
 # Load the dataset
-df = pd.read_csv('dataset.csv')
+#df = pd.read_csv('dataset.csv')
 
 classifier = pipeline("zero-shot-classification", model = "facebook/bart-large-mnli")
 
 
-#class_labels = df['labeltext'].unique().tolist()
-class_descriptions = ["pets", "cats", "dogs", "animals", "lions", "dolphins"]
-query = "a lazy cat"
+query = "pakistan and it's people"
+start_time = time.perf_counter()  # Start timing
+keywords = extract_keywords(query)
 
-result = classifier(query, class_descriptions, multi_label = True)
+for keyword in keywords:
+    synonym_list = find_synonyms(keyword)
+    if synonym_list:
+        if (len(synonym_list) > 5):
+            result = classifier(query, synonym_list[:5], multi_label = True)
+            print(result)
+        else:
+            result = classifier(query, synonym_list[:5], multi_label = True)
+            print(result)
+            
+    
+end_time = time.perf_counter()  # End timing
+duration = end_time - start_time  # Calculate duration
+print(f"Classification time: {duration:.6f} seconds")
 
-print(result)
+
+
+#result = classifier(query, class_descriptions, multi_label = True)
+
+
+
 
