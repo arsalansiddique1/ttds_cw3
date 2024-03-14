7b87cf4469c0702bbce7b2439ffc7e61ea69482a
arsalansiddique1 <arsalan1699@hotmail.com>
2024-03-14 17:32:57 +0000

    changed to a function

--- LLM_query_expansion/zero_shot.py
+++ LLM_query_expansion/zero_shot.py
@@ -6,33 +6,31 @@ from keywords_extraction import *
 
 # Load the dataset
 #df = pd.read_csv('dataset.csv')
-
-classifier = pipeline("zero-shot-classification", model = "facebook/bart-large-mnli")
-
-
-query = "a lazy cat"
-start_time = time.perf_counter()  # Start timing
-keywords = extract_keywords(query)
-rel_terms = []
-for keyword in keywords:
-    synonym_list = find_synonyms(keyword)
-    if synonym_list:
-        if (len(synonym_list) > 5):
-            result = classifier(query, synonym_list[:5], multi_label = True)
-            rel_terms.append(result['labels'][0])
-        else:
-            result = classifier(query, synonym_list[:5], multi_label = True)
-            rel_terms.append(result['labels'][0])
-            
-    
-end_time = time.perf_counter()  # End timing
-duration = end_time - start_time  # Calculate duration
-print(f"Classification time: {duration:.6f} seconds")
-print(rel_terms)
+def get_relevant_terms(query):
+    classifier = pipeline("zero-shot-classification", model = "facebook/bart-large-mnli")
 
 
+    start_time = time.perf_counter()  # Start timing
+    keywords = extract_keywords(query)
+    rel_terms = []
+    for keyword in keywords:
+        synonym_list = find_synonyms(keyword)
+        if synonym_list:
+            if (len(synonym_list) > 5):
+                result = classifier(query, synonym_list[:5], multi_label = True)
+                rel_terms.append(result['labels'][0])
+            else:
+                result = classifier(query, synonym_list[:5], multi_label = True)
+                rel_terms.append(result['labels'][0])
+                
+        
+    end_time = time.perf_counter()  # End timing
+    duration = end_time - start_time  # Calculate duration
+    #print(f"Classification time: {duration:.6f} seconds")
+    return rel_terms
 
-#result = classifier(query, class_descriptions, multi_label = True)
+
+result = print(get_relevant_terms("this is a test"))
 
 
 
